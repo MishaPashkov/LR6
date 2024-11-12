c7a2bfbe8406917d688fd75d8a6efeeed2986364
MishaPashkov <misapaskov23@gmail.com>
2024-11-12 17:55:26 +0200

    test

---
+++ LR6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "math.h"
+using namespace std;
+int main()
+{
+	double a, b, c, x, f;
+	bool pr = true;
+	cout << "Enter a, b, c, x\n";
+	cin >> a >> b >> c >> x;
+	if (a < 0 && x != 0) f = a * pow(x, 2) + pow(b, 2) * x;
+	else
+		if (a > 0 && x == 0) f = x - a / (x - c);
+		else
+			f = 1 + x / c;
+	if (pr == true) cout << "F = " << f;
+	else cout << "F is not defined ";
+}
